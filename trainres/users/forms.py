ee2ec0ed018360d5e32071dd45128d8be5416841
CatB1t <71686115+CatB1t@users.noreply.github.com>
2021-07-12 23:20:01 +0200

    Updated login to use custom form

--- trainres/users/forms.py
+++ trainres/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms.widgets import Widget
 
 class UserRegisterationForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +9,10 @@ class UserRegisterationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(request=request, *args, **kwargs)
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control', 'id':'username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control', 'id':'password'}))
--- trainres/users/urls.py
+++ trainres/users/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from . import forms
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='users-login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', authentication_form=forms.UserLoginForm), name='users-login'),
     path('register/', views.register, name='users-register'),
     path('logout/', views.logout_view, name='users-logout')
 ]
