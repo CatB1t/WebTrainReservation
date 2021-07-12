d762b7457628476b1e26dc51b7b970158ab2f9e5
CatB1t <71686115+CatB1t@users.noreply.github.com>
2021-07-12 17:45:03 +0200

    Added Register view and register template
    Added register view, updated proper related files

--- trainres/users/views.py
+++ trainres/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .forms import UserRegisterationForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.sucess()
+            return redirect('trainres-home')
+    else:
+        form = UserRegisterationForm()
+    return render(request, 'users/register.html', {'form': form})
