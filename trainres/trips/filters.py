75d6b66f68fb62fe2af74aacccae9093fe4a242b
CatB1t <71686115+CatB1t@users.noreply.github.com>
2021-07-13 18:43:40 +0200

    Added minimum slots filter, updated styling

--- trainres/trips/filters.py
+++ trainres/trips/filters.py
@@ -1,6 +1,6 @@
+import django_filters
 from django import forms
 from .models import Trip
-import django_filters
 
 class BookFilter(django_filters.FilterSet):
     source_choices = [(i['source'], i['source']) for i in Trip.objects.values('source').distinct()]
@@ -15,11 +15,18 @@ class BookFilter(django_filters.FilterSet):
         widget=forms.Select(attrs={'class':'form-control'}))
 
     dept_date = django_filters.DateFilter(input_formats=['%d-%m-%Y'], 
-    widget=forms.TextInput(attrs={'name': 'Departure Time','placeholder':'dd-mm-yyyy', 'class': 'form-control'}) ,
-    label="Departure Date")
+    widget=forms.TextInput(attrs={'placeholder':'dd-mm-yyyy', 'class': 'form-control'}) ,
+    label="Date")
 
-    dept_time = django_filters.TimeFilter(widget=forms.TextInput(attrs={'placeholder':'hh:mm', 'class': 'form-control'}), label="Departure Time")
+    dept_time = django_filters.TimeFilter(widget=forms.TextInput(attrs={'placeholder':'hh:mm', 'class': 'form-control'}), label="Time")
+
+    reserved_seats = django_filters.NumberFilter(
+        method='minSeat', widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        label="Minimum Slots")
+
+    def minSeat(self,qs,name,value):
+        return qs.filter(reserved_seats__gte=value)
     
     class Meta:
         model = Trip
-        fields = ['source', 'destination', 'dept_date', 'dept_time']
+        fields = ['source', 'destination', 'dept_date', 'dept_time', 'reserved_seats']
